694976ce9d4fa347229cace10237d898a51b32df
alanmdf <alanmoreirascr@gmail.com>
2021-06-07 18:53:09 -0300

    Requisito Obrigatorio 5 Concluido

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-window.onload = function() {
+window.onload = function () {
   const ballsSection = document.querySelector('.color-balls-section');
-  console.log(ballsSection);
+  const rgbColor = document.getElementById('rgb-color');
+  const answer = document.getElementById('answer');
+  answer.innerText = 'Escolha uma cor';
 
   createBalls();
 
-  function createBalls() {
+  function createBalls () {
     for (let index = 0; index < 6; index += 1) {
-      let ball = document.createElement('div');
+      const ball = document.createElement('div');
       ball.className = 'ball'
-      let color1 = parseInt((Math.random() * 255) + 1);
-      let color2 = parseInt((Math.random() * 255) + 1);
-      let color3 = parseInt((Math.random() * 255) + 1);
-      let color = `rgb(${color1}, ${color2}, ${color3})`;
-      console.log(color);
+      const color1 = parseInt((Math.random() * 255) + 1);
+      const color2 = parseInt((Math.random() * 255) + 1);
+      const color3 = parseInt((Math.random() * 255) + 1);
+      const color = `rgb(${color1}, ${color2}, ${color3})`;
       ball.style.backgroundColor = color;
       ballsSection.appendChild(ball);
+    }
+    rgbColor.innerText = ballsSection.children[0].style.backgroundColor
+  }
+
+  ballsSection.addEventListener('click', checkColor)
+
+  function checkColor (element) {
+    if (element.target.style.backgroundColor === rgbColor.innerText ) {
+      answer.innerText = 'Acertou!';
+    } else {
+      answer.innerText = 'Errou! Tente novamente!';
     }
   }
 }
